d26c4891b60ba55ef15817d43f325c5028436466
jjkil12 <d140490@kbfg.com>
2020-12-21 17:04:27 +0900

    gitignore

---
+++ src/main/java/com/kbds/aws1/springboot/web/dto/HelloResponseDto.java
@@ -0,0 +1,14 @@
+package com.kbds.aws1.springboot.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+
+    private final String name;
+    private final int amount;
+
+}
