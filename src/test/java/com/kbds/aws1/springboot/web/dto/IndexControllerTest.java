a0c65a41d1f589a1739347c6299cd35a36b1c622
jjkil12 <d140490@kbfg.com>
2020-12-27 14:03:16 +0900

    gitignore

--- src/test/java/com/kbds/aws1/springboot/web/dto/IndexControllerTest.java
+++ src/test/java/com/kbds/aws1/springboot/web/dto/IndexControllerTest.java
@@ -23,7 +23,7 @@ public class IndexControllerTest {
         String body = this.restTemplate.getForObject("/", String.class);
 
         //then
-        assertThat(body).contains("KB데이타 AWS COP");
+        assertThat(body).contains("KB DATA SYSTEM COP");
 
     }
 }
