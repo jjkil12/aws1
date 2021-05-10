af5c766f19b22bab91c0a42ce8976c67a0866c05
jjkil12 <d140490@kbfg.com>
2021-05-10 22:27:44 +0900

    gitignore

--- src/main/java/com/kbds/aws1/springboot/web/IndexController.java
+++ src/main/java/com/kbds/aws1/springboot/web/IndexController.java
@@ -1,8 +1,10 @@
 package com.kbds.aws1.springboot.web;
 
+import ch.qos.logback.core.CoreConstants;
 import com.kbds.aws1.springboot.config.auth.LoginUser;
 import com.kbds.aws1.springboot.config.auth.dto.SessionUser;
 import com.kbds.aws1.springboot.service.posts.PostsService;
+import com.kbds.aws1.springboot.web.dto.PostsListResponseDto;
 import com.kbds.aws1.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -11,6 +13,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import javax.servlet.http.HttpSession;
+import java.util.Iterator;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Controller
@@ -22,12 +26,37 @@ public class IndexController {
     @GetMapping("/")
     public String index(Model model, @LoginUser SessionUser user) {
     //public String index(Model model) {
-        model.addAttribute("posts", postsService.findAllDesc());
+
+        List<PostsListResponseDto> list1 = postsService.findAllDesc();
+
+        Iterator iterator = list1.iterator();
+
+        while (iterator.hasNext()) {
+            PostsListResponseDto strCol = (PostsListResponseDto) iterator.next();
+            if("[COP]".equals(strCol.getTitle().substring(0,5))) {
+                strCol.setColor("#6f42c1");
+            }
+            else {
+                strCol.setColor("#007bff");
+            }
+        }
+        model.addAttribute("posts", list1);
         //SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        //Model model1 = (Model) model.getAttribute("posts");
 
         if (user != null) {
             model.addAttribute("userName", user.getName());
             model.addAttribute("userEmail", user.getEmail());
+/*
+            if(model1.getAttribute("title").toString().toLowerCase().charAt(0) == '[') {
+                model.addAttribute("color", "#e83e8c");
+            }
+            else {
+                model.addAttribute("color", "#007bff");
+            }
+
+ */
         }
         return "index";
     }
